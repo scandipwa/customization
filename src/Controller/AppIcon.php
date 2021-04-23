5384f7fa51df6aa8b23420fc6d9a7883f9bcef13
Zans Laksa <zans.laksa@scandiweb.com>
2021-04-23 13:28:51 +0300

    See scandipwa/scandipwa#2434 Added maskable icon in order to fix lighthouse report error

--- src/Controller/AppIcon.php
+++ src/Controller/AppIcon.php
@@ -24,6 +24,8 @@ class AppIcon
 
     const STORAGE_PATH = 'favicon/icons/';
 
+    const MASKABLE_IMAGE_SIZE = 192;
+
     const IMAGE_RESIZING_CONFIG = [
         'ios' => [
             'type' => 'ios',
@@ -118,10 +120,12 @@ class AppIcon
                 $height = is_array($size) ? $size[1] : $size;
                 $name = 'icon_' . $config['type'] . '_' . $width . 'x' . $height;
                 $src = '../' . self::STORAGE_PATH . $name . '.png';
+                $purpose = $width === self::MASKABLE_IMAGE_SIZE ? 'any maskable' : 'any';
                 $output[] = [
                     'src' => $src,
                     'type' => 'image/png',
-                    'sizes' => $width . 'x' . $height
+                    'sizes' => $width . 'x' . $height,
+                    'purpose' => $purpose
                 ];
             }
         }
