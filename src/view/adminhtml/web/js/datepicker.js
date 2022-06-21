34e1cd4b1078cbb5bc0e9f5ae76ac52374c45b81
Azizbek Rasulov <azizbek.rasulov@scandiweb.com>
2022-06-21 13:03:55 +0500

    CR changes

--- src/view/adminhtml/web/js/datepicker.js
+++ src/view/adminhtml/web/js/datepicker.js
@@ -1,22 +1,21 @@
 //This script fixes incorrect positioning in modal datepickers.
 //This file should be removed after magento fixes the datepickers.
 
-require(["jquery"], function ($) {
-    $(window).on("load", function () {
-        if (window.location.pathname.includes("/admin/catalog/product/edit/id")) {
-            (($) => {
-                const show = $.datepicker._showDatepicker;
-                const datePicker = $($.datepicker.dpDiv[0]);
-                $.extend($.datepicker, {
-                    _showDatepicker: (trigger) => {
-                        show(trigger);
+require(['jquery'], ($) => {
+    $(window).on('load', () => {
+        const show = $.datepicker._showDatepicker;
+        if (show) {
+            const datePicker = $($.datepicker.dpDiv[0]);
 
-                        if ($(trigger).parents(".modal-slide").length > 0) {
-                            datePicker.css("top", `${datePicker.height() - 25}px`);
-                        }
+            $.extend($.datepicker, {
+                _showDatepicker: (trigger) => {
+                    show(trigger);
+
+                    if ($(trigger).parents('.modal-slide').length > 0) {
+                        datePicker.css('top', `${datePicker.height() - 25}px`);
                     }
-                });
-            })(jQuery);
+                },
+            });
         }
     });
 });
